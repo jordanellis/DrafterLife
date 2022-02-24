63e8b53c4d324bff9d37a6fa3cda149e07c20397
jordanellis <jordanellis620@gmail.com>
2022-02-24 17:38:46 +0000

    Upadates to run in heroku

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 var app = express();
@@ -29,7 +28,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true })
     });
 
     app.get('/*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'public', 'index.html'), function(err) {
+      res.sendFile('build/index.html', function(err) {
         if (err) {
           res.status(500).send(err)
         }
