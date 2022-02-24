4d86f5e50274f7d7a126007459c9d1bdb20926ab
jordanellis <jordanellis620@gmail.com>
2022-02-24 16:02:30 +0000

    Updated port value in express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path")
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 var app = express();
@@ -10,7 +9,7 @@ const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@c
 MongoClient.connect(uri, { useUnifiedTopology: true })
   .then(client => {
     app.db = client.db("drafterlife")
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static('build'));
     app.use(express.json());
     app.use("/api/games", require("./games/v1"));
     app.use("/api/league", require("./league/v1"));
